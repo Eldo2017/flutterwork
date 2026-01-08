fd686d85d3babd33948264590ebdd5e42072442a
lealle <satoru31572@gmail.com>
2026-01-08 15:55:42 +0900

    최종 학습

--- flutter/flutter06_springboot/lib/services/db_service.dart
+++ flutter/flutter06_springboot/lib/services/db_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+
+import '../models/user_model.dart';
 
 class DbService {
   final String BaseURL = "http://172.16.251.198:8080"; // 스프링부트 API
@@ -13,6 +14,28 @@ class DbService {
       return jsonDecode(response.body)['isAvailable'];
     } else {
       throw Exception("이메일 확인 실패");
+    }
+  }
+
+  Future<void> registerUser(User user) async {
+    final response = await http.post(
+      Uri.parse("$BaseURL/flutter/insert"),
+      headers: {'Content-Type' : 'application/json'},
+      body: jsonEncode(user.toJson())
+    );
+
+    if(response.statusCode != 200) {
+      throw Exception("회원가입에 실패하였습니다");
+    }
+
+    Future<List<User>> getAllUSers() async {
+      final response = await http.get(Uri.parse("$BaseURL/flutter/users"));
+      if(response.statusCode == 200) {
+        List<dynamic> userList = jsonDecode(response.body);
+        return userList.map((u) => User.fromJson(u)).toList();
+      } else {
+        throw Exception("모든 사용자 정보를 가져오지 못했습니다 : ${response.statusCode}");
+      }
     }
   }
 }
